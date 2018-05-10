8edecea7afa7e8dfa9cbd322eefeb1e0b2135ee3
xuanbingji <hjunzhen@163.com>
2018-05-10 17:48:30 +0800

    xianxinghuigui

---
+++ xianxinghuigui.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2018-05-10 17:46:07
+# @Author  : Your Name (you@example.org)
+# @Link    : http://example.org
+# @Version : $Id$
+
+import os
